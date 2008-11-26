57c9644e837d8b69c435617629060b96f3629fef
Łukasz Serwatka <ls@ez.no>
2008-11-26 09:41:16 +0000

    - Added fin-FI translation
    git-svn-id: http://svn.projects.ez.no/ezmultiupload/trunk@13 d44cdf02-dba6-4dc1-9069-7147f5ab4e2f

---
+++ translations/fin-FI/translation.ts
@@ -0,0 +1,41 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>extension/ezmultiupload</name>
+    <message>
+        <source>Error</source>
+        <translation>Virhe</translation>
+    </message>
+    <message>
+        <source>Starting...</source>
+        <translation>Aloittaa...</translation>
+    </message>
+    <message>
+        <source>All files received.</source>
+        <translation>Kaikki tiedostot vastaanotettu.</translation>
+    </message>
+    <message>
+        <source>Unable to load Flash content. You can download the latest version of Flash Player from the</source>
+        <translation>Flash-sisällön lataus epäonnistui. Voit asentaa viimeisimmän versoin Flash Playeristä osoitteesta</translation>
+    </message>
+    <message>
+        <source>Adobe Flash Player Download Center</source>
+        <translation>Adobe Flash Playerin Latauskeskus</translation>
+    </message>
+    <message>
+        <source>The files are uploaded to</source>
+        <translation>Tiedostot on ladattu osoitteeseen</translation>
+    </message>
+    <message>
+        <source>Select files</source>
+        <translation>Valitse tiedostot</translation>
+    </message>
+    <message>
+        <source>Thumbnail created.</source>
+        <translation>Esikatselukuva luotu.</translation>
+    </message>
+    <message>
+        <source>Multiupload</source>
+        <translation>Monilataus</translation>
+    </message>
+</context>
+</TS>
