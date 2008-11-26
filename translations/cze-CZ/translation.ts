9179b24a7a8d036f04b861fb560cc0b9557f4304
Łukasz Serwatka <ls@ez.no>
2008-11-26 12:30:43 +0000

    - Added cze-CZ translation
    git-svn-id: http://svn.projects.ez.no/ezmultiupload/trunk@14 d44cdf02-dba6-4dc1-9069-7147f5ab4e2f

---
+++ translations/cze-CZ/translation.ts
@@ -0,0 +1,41 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>extension/ezmultiupload</name>
+    <message>
+        <source>Multiupload</source>
+        <translation>Hromadná nahrávka</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Chyba</translation>
+    </message>
+    <message>
+        <source>The files are uploaded to</source>
+        <translation>Soubory jsou nahrány do</translation>
+    </message>
+    <message>
+        <source>Select files</source>
+        <translation>Vybrat soubory</translation>
+    </message>
+    <message>
+        <source>Unable to load Flash content. You can download the latest version of Flash Player from the</source>
+        <translation>Není možné nahrát obsah Flash. Můžete nahrát poslední verzi Flash Player z </translation>
+    </message>
+    <message>
+        <source>Adobe Flash Player Download Center</source>
+        <translation>Adobe Flash Player Download Center</translation>
+    </message>
+    <message>
+        <source>All files received.</source>
+        <translation>Všechny soubory přijaty.</translation>
+    </message>
+    <message>
+        <source>Thumbnail created.</source>
+        <translation>Miniatura vytvořena.</translation>
+    </message>
+    <message>
+        <source>Starting...</source>
+        <translation>Spouští se...</translation>
+    </message>
+</context>
+</TS>
