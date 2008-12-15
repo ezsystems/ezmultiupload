500964937f1836fc17f5038e772a76fca8ed0e18
Łukasz Serwatka <ls@ez.no>
2008-12-15 09:58:15 +0000

    - Updated license headers
    git-svn-id: http://svn.projects.ez.no/ezmultiupload/trunk@19 d44cdf02-dba6-4dc1-9069-7147f5ab4e2f

--- classes/ezmultiuploadhandler.php
+++ classes/ezmultiuploadhandler.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * File containing the eZMultiuploadHandler class.
+ *
+ * @copyright Copyright (C) 2005-2008 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPL v2
+ * @version //autogentag//
+ * @package ezmultiupload
+ */
 
 /**
  * eZMultiuploadHandler class
--- classes/ezmultiuploadhandlerinterface.php
+++ classes/ezmultiuploadhandlerinterface.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * File containing the eZMultiuploadHandlerInterface interface.
+ *
+ * @copyright Copyright (C) 2005-2008 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPL v2
+ * @version //autogentag//
+ * @package ezmultiupload
+ */
 
 interface eZMultiuploadHandlerInterface
 {
