280d2dabbff232d17e4f16e973f8460119f9038a
Łukasz Serwatka <ls@ez.no>
2008-12-15 09:36:45 +0000

    - Updated ezmultiuploadhandler class
    git-svn-id: http://svn.projects.ez.no/ezmultiupload/trunk@18 d44cdf02-dba6-4dc1-9069-7147f5ab4e2f

--- classes/ezmultiuploadhandler.php
+++ classes/ezmultiuploadhandler.php
@@ -1,21 +1,34 @@
 <?php
 
+/**
+ * eZMultiuploadHandler class
+ *
+ */
 class eZMultiuploadHandler
 {
+    /**
+     * Call preUpload or postUpload user definied functions.
+     *
+     * @static
+     * @param string $method
+     * @param array $result
+     * @return bool
+     */
     static function exec( $method, &$result )
     {
         $ini = eZINI::instance( 'ezmultiupload.ini' );
-
         $handlers = $ini->variable( 'MultiUploadSettings', 'MultiuploadHandlers' );
+
+        if ( !$handlers )
+            return false;
 
         foreach ( $handlers as $hanlder )
         {
-            $obj = new $hanlder();
-            if ( $obj )
-                $obj->$method( $result );
-            else
+            if ( !call_user_func( array( $hanlder, $method ), &$result ) )
                 eZDebug::writeWarning( 'Multiupload handler implementation not found' );
         }
+
+        return true;
     }
 }
 
