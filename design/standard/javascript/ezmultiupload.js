07631185548be12fe86879b9cc88ac632dcc7328
Łukasz Serwatka <ls@ez.no>
2009-02-17 15:43:50 +0000

    - Return current str if escaping fail - Some cfg changes
    git-svn-id: http://svn.projects.ez.no/ezmultiupload/trunk@25 d44cdf02-dba6-4dc1-9069-7147f5ab4e2f

--- design/standard/javascript/ezmultiupload.js
+++ design/standard/javascript/ezmultiupload.js
@@ -7,14 +7,14 @@ YAHOO.ez.MultiUpload = (function() {
     var Dom = YAHOO.util.Dom,
         Event = YAHOO.util.Event,
         Connect = YAHOO.util.Connect,
-        JSON = YAHOO.lang.JSON
+        JSON = YAHOO.lang.JSON;
 
-    var onContentReady = function(o, cfg) {
+    var onContentReady = function(o) {
         this.setAllowMultipleFiles(true);
-        this.setFileFilters(cfg.fileType);
+        this.setFileFilters(YAHOO.ez.MultiUpload.cfg.fileType);
     };
 
-    var onFileSelect = function(e, cfg) {
+    var onFileSelect = function(e) {
         Dom.setStyle('multiuploadProgress' , 'display', 'block');
         if( Dom.getStyle('multiuploadProgress' , 'opacity' ) == 0) {
            fadeAnimate('multiuploadProgress' , 0, 1);
@@ -27,17 +27,14 @@ YAHOO.ez.MultiUpload = (function() {
         }
 
         var fileID = this.queue[this.uploadCounter]['id'];
-        this.upload(fileID, cfg.uploadURL, 'POST', cfg.uploadVars);
+        this.upload(fileID, YAHOO.ez.MultiUpload.cfg.uploadURL, 'POST', YAHOO.ez.MultiUpload.cfg.uploadVars);
         
         Dom.get('uploadButton').disabled = true;
         
         var cancelUploadButton = Dom.get('cancelUploadButton');
         Dom.setStyle(cancelUploadButton, 'visibility', 'visible');
         
-        Event.on(cancelUploadButton, 'click', cancelUpload, {
-            uploader: this,
-            cfg: cfg
-        }, true );
+        Event.on(cancelUploadButton, 'click', cancelUpload, this, true);
         
         this.disable();
     };
@@ -49,27 +46,27 @@ YAHOO.ez.MultiUpload = (function() {
         Dom.get('multiuploadProgressFileName').innerHTML = this.queue[this.uploadCounter]['name'];
     };
 
-    var onUploadProgress = function(e, cfg) {
-        var width = Math.floor((cfg.progressBarWidth * e['bytesLoaded']) / e['bytesTotal']);
+    var onUploadProgress = function(e) {
+        var width = Math.floor((YAHOO.ez.MultiUpload.cfg.progressBarWidth * e['bytesLoaded']) / e['bytesTotal']);
 
         widthAnimate('multiuploadProgressBar', width);
     };
 
-    var onUploadComplete = function(e, cfg) {
-        Dom.get('multiuploadProgressMessage').innerHTML = cfg.thumbnailCreated;
+    var onUploadComplete = function(e) {
+        Dom.get('multiuploadProgressMessage').innerHTML = YAHOO.ez.MultiUpload.cfg.thumbnailCreated;
         Dom.setStyle('multiuploadProgressBar' , 'width', '100%');
         
         if (this.uploadCounter < this.queue.length - 1) {
             this.uploadCounter++;
 
             var fileID = this.queue[this.uploadCounter]['id'];
-            this.upload(fileID, cfg.uploadURL, 'POST', cfg.uploadVars);
+            this.upload(fileID, YAHOO.ez.MultiUpload.cfg.uploadURL, 'POST', YAHOO.ez.MultiUpload.cfg.uploadVars);
         } else {
             this.uploadCounter = 0;
             this.queue = [];
 
             Dom.get('uploadButton').disabled = false;
-            Dom.get('multiuploadProgressMessage').innerHTML = cfg.allFilesRecived;
+            Dom.get('multiuploadProgressMessage').innerHTML = YAHOO.ez.MultiUpload.cfg.allFilesRecived;
 
             Dom.setStyle('cancelUploadButton', 'visibility', 'hidden');
             
@@ -78,7 +75,7 @@ YAHOO.ez.MultiUpload = (function() {
         }
     };
 
-    var onUploadCompleteData = function(e, cfg) {
+    var onUploadCompleteData = function(e) {
         var response = JSON.parse(e.data);
         var id = response.id;
         var data = unescapeHTML(response.data);
@@ -95,14 +92,14 @@ YAHOO.ez.MultiUpload = (function() {
     };
 
     var cancelUpload = function(){
-        this.uploader.cancel();
-        this.uploader.enable();
-        this.uploader.clearFileList();
+        this.cancel();
+        this.enable();
+        this.clearFileList();
         
-        this.uploader.uploadCounter = 0;
-        this.uploader.queue = [];
+        this.uploadCounter = 0;
+        this.queue = [];
         
-        Dom.get('multiuploadProgressMessage').innerHTML = this.cfg.uploadCanceled;
+        Dom.get('multiuploadProgressMessage').innerHTML = YAHOO.ez.MultiUpload.cfg.uploadCanceled;
         Dom.get('uploadButton').disabled = false;
     };
 
@@ -123,7 +120,7 @@ YAHOO.ez.MultiUpload = (function() {
     var unescapeHTML = function(s) {
         var div = document.createElement('div');
         div.innerHTML = stripTags(s);
-        return div.childNodes[0] ? div.childNodes[0].nodeValue : '';
+        return div.childNodes[0] ? div.childNodes[0].nodeValue : s;
     };
 
     // Public
@@ -138,15 +135,15 @@ YAHOO.ez.MultiUpload = (function() {
                 Dom.setStyle(overlay, 'width', uploadButton.right - uploadButton.left + "px");
                 Dom.setStyle(overlay, 'height', uploadButton.bottom - uploadButton.top + "px");
                 
-                YAHOO.widget.Uploader.SWFURL = this.cfg.swfURL;
+                YAHOO.widget.Uploader.SWFURL = YAHOO.ez.MultiUpload.cfg.swfURL;
                 var uploader = new YAHOO.widget.Uploader('uploadButtonOverlay');
             
-                uploader.on('contentReady', onContentReady, this.cfg, false);
-                uploader.on('fileSelect', onFileSelect, this.cfg, false);
+                uploader.on('contentReady', onContentReady);
+                uploader.on('fileSelect', onFileSelect);
                 uploader.on('uploadStart', onUploadStart);
-                uploader.on('uploadProgress', onUploadProgress, this.cfg, false);
-                uploader.on('uploadComplete', onUploadComplete, this.cfg, false);
-                uploader.on('uploadCompleteData', onUploadCompleteData, this.cfg, false);
+                uploader.on('uploadProgress', onUploadProgress);
+                uploader.on('uploadComplete', onUploadComplete);
+                uploader.on('uploadCompleteData', onUploadCompleteData);
                 uploader.on('uploadError', onUploadError);
                 
                 uploader.uploadCounter = 0;
