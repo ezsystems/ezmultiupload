06d5213e1a6b287cd3cae327c713b801f967854c
Łukasz Serwatka <ls@ez.no>
2009-03-17 10:55:29 +0000

    - Updated dist file.
    git-svn-id: http://svn.projects.ez.no/ezmultiupload/trunk@29 d44cdf02-dba6-4dc1-9069-7147f5ab4e2f

--- dist.sh
+++ dist.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-EXTENSION_NAME="eZ Multiupload Extension"
+EXTENSION_NAME="eZ Multiupload"
 EXTENSION_IDENTIFIER="ezmultiupload"
-EXTENSION_SUMMARY="This extension enables users to upload multiple files at once via the front-end of an eZ Publish website."
+EXTENSION_SUMMARY="The eZ Multiupload extension enables users to upload multiple files such as images, documents, binary files via the front-end of an eZ Publish website. When used together with the eZODF extension, users can import multiple OpenDocument Text or Word Documents as article objects at once."
 EXTENSION_LICENSE="GNU General Public License v2.0"
 EXTENSION_VERSION="1.0.0"
-EXTENSION_PUBLISH_VERSION="4.0 and higher"
+EXTENSION_PUBLISH_VERSION="4.1 and higher"
 EXTENSION_ARCHIVE_NAME="ezmultiupload"
 EXTENSION_PHP_VERSION="5.1"
 
