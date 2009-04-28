eab1f049ac433e84bb0de833ce8edd7a5edc085d
Łukasz Serwatka <ls@ez.no>
2009-04-28 11:27:23 +0000

    - Updated versions
    git-svn-id: http://svn.projects.ez.no/ezmultiupload/stable/1.0@36 d44cdf02-dba6-4dc1-9069-7147f5ab4e2f

--- modules/ezmultiupload/upload.php
+++ modules/ezmultiupload/upload.php
@@ -4,7 +4,7 @@
  *
  * @copyright Copyright (C) 2005-2009 eZ Systems AS. All rights reserved.
  * @license http://ez.no/licenses/gnu_gpl GNU GPL v2
- * @version 1.0.0
+ * @version 1.0.1
  * @package ezmultiupload
  */
 
