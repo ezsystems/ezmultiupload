aebd3fd1a56b475b3d46e9ebee3953c24e21f687
Damien Pobel <dpobel@free.fr>
2011-03-08 17:06:57 +0800

    Localization: updated base translations.ts file

--- translations/untranslated/translation.ts
+++ translations/untranslated/translation.ts
@@ -1,4 +1,13 @@
-<!DOCTYPE TS><TS>
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>design/admin/popupmenu</name>
+    <message>
+        <source>Upload multiple files</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 <context>
     <name>extension/ezmultiupload</name>
     <message>
@@ -18,23 +27,11 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Starting...</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <source>Thumbnail created.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>All files received.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Unable to load Flash content. You can download the latest version of Flash Player from the</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Adobe Flash Player Download Center</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -45,11 +42,16 @@
         <source>Upload canceled.</source>
         <translation type="unfinished"></translation>
     </message>
-</context>
-<context>
-    <name>design/admin/popupmenu</name>
     <message>
-        <source>Upload multiple files</source>
+        <source>Allowed Files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Could not load flash(or not loaded yet), this is needed for multiupload!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Javascript has been disabled, this is needed for multiupload!</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
