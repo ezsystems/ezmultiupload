31d88f983f5fab816794a3d78d118edf00298278
Patrick Allaert <pa@ez.no>
2011-03-15 14:52:27 +0100

    Changed: (C) year: welcome 2011

--- ezinfo.php
+++ ezinfo.php
@@ -2,7 +2,7 @@
 /**
  * File containing the ezmultiuploadInfo class.
  *
- * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
+ * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
  * @license http://ez.no/licenses/gnu_gpl GNU GPL v2
  * @version 1.1.0
  * @package ezmultiupload
@@ -14,7 +14,7 @@ class ezmultiuploadInfo
     {
         return array( 'Name' => 'eZ Multiupload',
                       'Version' => '//autogentag//',
-                      'Copyright' => 'Copyright (C) 1999-2010 eZ Systems AS',
+                      'Copyright' => 'Copyright (C) 1999-2011 eZ Systems AS',
                       'License' => 'GNU General Public License v2.0',
                     );
     }
