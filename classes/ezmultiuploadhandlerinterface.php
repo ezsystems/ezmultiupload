15fa409b0319c8e3fe1330123fce63ba98047799
Patrick Allaert <pa@ez.no>
2011-03-15 14:36:49 +0100

    Changed: (C) year: welcome 2011

--- classes/ezmultiuploadhandlerinterface.php
+++ classes/ezmultiuploadhandlerinterface.php
@@ -2,7 +2,7 @@
 /**
  * File containing the eZMultiuploadHandlerInterface interface.
  *
- * @copyright Copyright (C) 2005-2009 eZ Systems AS. All rights reserved.
+ * @copyright Copyright (C) 2005-2011 eZ Systems AS. All rights reserved.
  * @license http://ez.no/licenses/gnu_gpl GNU GPL v2
  * @version 1.0.0
  * @package ezmultiupload
