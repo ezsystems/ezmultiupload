6dc57034cc1d161387d121a57a3a1b592187600b
Edi Modric <edi.modric@gmail.com>
2011-11-08 19:10:23 +0000

    Croatian translation

---
+++ translations/cro-HR/translation.ts
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="hr_HR">
+<context>
+    <name>design/admin/popupmenu</name>
+    <message>
+        <source>Upload multiple files</source>
+        <translation>Učitaj više datoteka</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezmultiupload</name>
+    <message>
+        <source>Multiupload</source>
+        <translation>Učitavanje više datoteka</translation>
+    </message>
+    <message>
+        <source>The files are uploaded to</source>
+        <translation>Datoteke su učitane u</translation>
+    </message>
+    <message>
+        <source>Select files</source>
+        <translation>Odaberite datoteke</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Greška</translation>
+    </message>
+    <message>
+        <source>Thumbnail created.</source>
+        <translation>Prikaz kreiran.</translation>
+    </message>
+    <message>
+        <source>All files received.</source>
+        <translation>Sve datoteke primljene.</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Odustani</translation>
+    </message>
+    <message>
+        <source>Upload canceled.</source>
+        <translation>Učitavanje prekinuto.</translation>
+    </message>
+    <message>
+        <source>Allowed Files</source>
+        <translation>Dopuštene datoteke</translation>
+    </message>
+    <message>
+        <source>Could not load flash(or not loaded yet), this is needed for multiupload!</source>
+        <translation>Flash komponentu potrebnu za višestruko učitavanje nije moguće pokrenuti (ili još nije pokrenuta)!</translation>
+    </message>
+    <message>
+        <source>Javascript has been disabled, this is needed for multiupload!</source>
+        <translation>Podrška za Javascript je onemogućena, no potrebna je za višestruko učitavanje!</translation>
+    </message>
+</context>
+</TS>
