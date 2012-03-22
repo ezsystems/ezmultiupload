5ab3f1a85c91db6d55f733165be6888c96c9f0e0
Tutei Admin <thiagocamposviana@gmail.com>
2012-03-23 00:07:45 +0100

    Finalized pt-BR translation

---
+++ translations/por-BR/translation.ts
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>design/admin/popupmenu</name>
+    <message>
+        <source>Upload multiple files</source>
+        <translation>Enviar vários arquivos</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezmultiupload</name>
+    <message>
+        <source>Multiupload</source>
+        <translation>Multiupload</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Erro</translation>
+    </message>
+    <message>
+        <source>The files are uploaded to</source>
+        <translation>Os arquivos são enviados para</translation>
+    </message>
+    <message>
+        <source>Select files</source>
+        <translation>Selecionar arquivos</translation>
+    </message>
+    <message>
+        <source>Unable to load Flash content. You can download the latest version of Flash Player from the</source>
+        <translation type="obsolete">Não foi possível carregar o conteúdo em Flash. Você pode baixar a última versão do Flash Player em</translation>
+    </message>
+    <message>
+        <source>Adobe Flash Player Download Center</source>
+        <translation type="obsolete">Central de download do Adobe Flash Player</translation>
+    </message>
+    <message>
+        <source>All files received.</source>
+        <translation>Todos arquivos foram recebidos.</translation>
+    </message>
+    <message>
+        <source>Thumbnail created.</source>
+        <translation>Miniatura criada.</translation>
+    </message>
+    <message>
+        <source>Starting...</source>
+        <translation type="obsolete">Iniciando...</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <source>Upload canceled.</source>
+        <translation>Envio cancelado.</translation>
+    </message>
+    <message>
+        <source>Allowed Files</source>
+        <translation>Arquivos permitidos</translation>
+    </message>
+    <message>
+        <source>Could not load flash(or not loaded yet), this is needed for multiupload!</source>
+        <translation>Não foi possível carregar o flash(ou não foi carregado ainda), ele é necessário para o multiupload!</translation>
+    </message>
+    <message>
+        <source>Javascript has been disabled, this is needed for multiupload!</source>
+        <translation>Javascript foi desabilitado, ele é necessário para o multiupload!</translation>
+    </message>
+</context>
+</TS>
