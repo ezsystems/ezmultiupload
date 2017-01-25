186a314bd791a68459dfcebd55b0e334cac66640
Gunnstein Lye <gunnstein.lye@ez.no>
2017-01-25 11:20:04 +0100

    EZP-26870: Upgrade ezmultiupload to YUI 3 (#16)
    (cherry picked from commit d48400f4f3d02fb5fd4a795223ea1bc0fa139130)
    (cherry picked from commit 7e8eb8c5f8adfa49f182901bc155306fdf6bfd6a)
    (cherry picked from commit a160af5bb36729234e0fc85425e6c31f09e0e82c)
    (cherry picked from commit 05703660f76eccde3a34ece857c967a6b399a9c3)

--- design/standard/javascript/ezmultiupload.js
+++ design/standard/javascript/ezmultiupload.js
@@ -1,191 +1,183 @@
-YAHOO.namespace('ez');
+YUI.add('ezmultiupload', function (Y) {
+    Y.namespace('ez');
 
-YAHOO.ez.MultiUpload = (function()
-{
-    // Private
+    Y.ez.MultiUpload = (function () {
+        // Private
 
-    var Dom = YAHOO.util.Dom,
-        Event = YAHOO.util.Event,
-        Connect = YAHOO.util.Connect,
-        JSON = YAHOO.lang.JSON;
+        var onFileSelect = function (e) {
+            Y.ez.MultiUpload.originalFileList = e.fileList;
 
-    var onContentReady = function(o)
-    {
-        this.setAllowMultipleFiles(true);
-        if ( YAHOO.ez.MultiUpload.cfg.fileType[0].extensions )
-            this.setFileFilters(YAHOO.ez.MultiUpload.cfg.fileType);
+            Y.one('#multiuploadProgress').setStyle('display', 'block');
 
-        Event.removeListener( Dom.get('uploadButtonOverlay'), 'click', missingFlashClick );
-    };
+            if (!Y.one('#multiuploadProgress').getStyle('opacity')) {
+                fadeAnimate('#multiuploadProgress', 0, 1);
+            }
+            Y.one('#multiuploadProgressBar').setStyle('width', 0);
+            Y.one('#multiuploadProgressMessage').setHTML('');
 
-    var missingFlashClick = function(o)
-    {
-        if ( YAHOO.ez.MultiUpload.cfg.flashError )
-            alert( YAHOO.ez.MultiUpload.cfg.flashError );
-        else
-            alert( "Could not load flash(or not yet loaded), this is needed for multiupload!" );
-    };
+            Y.ez.MultiUpload.uploader.uploadAll(Y.ez.MultiUpload.cfg.uploadURL, Y.ez.MultiUpload.cfg.uploadVars);
 
-    var onFileSelect = function(e)
-    {
-        Dom.setStyle('multiuploadProgress' , 'display', 'block');
-        if( Dom.getStyle('multiuploadProgress' , 'opacity' ) == 0)
-        {
-           fadeAnimate('multiuploadProgress' , 0, 1);
+            Y.one('#uploadButton').disabled = true;
+
+            var cancelUploadButton = Y.one('#cancelUploadButton');
+            cancelUploadButton.setStyle('visibility', 'visible');
+            cancelUploadButton.on('click', cancelUpload, this, true);
+
+            Y.ez.MultiUpload.uploader.disable();
+        };
+
+        var onUploadStart = function (e) {
+            Y.one('#multiuploadProgressBar').setStyle('width', 0);
+            Y.one('#multiuploadProgressMessage').setHTML('');
+            Y.one('#multiuploadProgressFile').setHTML('0/' + Y.ez.MultiUpload.originalFileList.length);
+        };
+
+        var onUploadProgress = function (e) {
+            var i;
+            for (i = 0; i < Y.ez.MultiUpload.originalFileList.length; i++) {
+                if (Y.ez.MultiUpload.originalFileList[i].get('name') == e.file.get('name')) {
+                    break;
+                }
+            }
+
+            Y.one('#multiuploadProgressFile').setHTML((i + 1) + '/' + Y.ez.MultiUpload.originalFileList.length);
+            Y.one('#multiuploadProgressFileName').setHTML(e.file.get('name'));
+        };
+
+        var onTotalUploadProgress = function (e) {
+            widthAnimate('#multiuploadProgressBar', e.percentLoaded + '%');
+        };
+
+        var onUploadComplete = function (e) {
+            Y.one('#multiuploadProgressMessage').setHTML(Y.ez.MultiUpload.cfg.thumbnailCreated);
+            Y.one('#multiuploadProgressBar').setStyle('width', '100%');
+
+            Y.one('#uploadButton').disabled = false;
+            Y.one('#multiuploadProgressMessage').setHTML(Y.ez.MultiUpload.cfg.allFilesRecived);
+
+            var fileCount = Y.ez.MultiUpload.originalFileList.length;
+            Y.one('#multiuploadProgressFile').setHTML(fileCount + '/' + fileCount);
+            Y.one('#multiuploadProgressFileName').setHTML('');
+
+            Y.one('#cancelUploadButton').setStyle('visibility', 'hidden');
+
+            Y.ez.MultiUpload.uploader.enable();
+            Y.ez.MultiUpload.uploader.set('fileList', []);
+
+            try {
+                var response = Y.JSON.parse(e.data);
+            }
+            catch (e) {
+                alert("Invalid JSON data");
+            }
+            var id = response.id;
+            var data = unescapeHTML(response.data);
+
+            var thumbnail = Y.Node.create('<div>' + data + '</div>');
+            thumbnail.addClass("thumbnail-block");
+            thumbnail.setAttribute("id", "thumbnail_" + id);
+            thumbnail.setStyle("opacity", 0);
+
+            Y.one('#thumbnails').appendChild(thumbnail);
+
+            fadeAnimate('#thumbnail_' + id, 0, 1);
+        };
+
+        var onUploadError = function (e) {
+            alert(e.status + ': ' + e.statusText);
+            console.log(e.status + ': ' + e.statusText);
+        };
+
+        var cancelUpload = function () {
+            Y.ez.MultiUpload.uploader.queue.cancelUpload();
+            Y.ez.MultiUpload.uploader.enable();
+            Y.ez.MultiUpload.uploader.set('fileList', []);
+
+            Y.one('#multiuploadProgressMessage').setHTML(Y.ez.MultiUpload.cfg.uploadCanceled);
+            Y.one('#uploadButton').disabled = false;
+        };
+
+        var fadeAnimate = function (elementID, fromValue, toValue) {
+            var anim = new Y.Anim({
+                node: elementID,
+                from: {
+                    opacity: fromValue
+                },
+                to: {
+                    opacity: toValue
+                },
+                duration: 0.2
+            });
+            anim.run();
+        };
+
+        var widthAnimate = function (elementID, toValue) {
+            var anim = new Y.Anim({
+                node: elementID,
+                to: {
+                    width: toValue
+                },
+                duration: 0.5
+            });
+            anim.run();
+        };
+
+        var stripTags = function (s) {
+            return s.replace(/<\/?[^>]+>/gi, '');
+        };
+
+        var unescapeHTML = function (s) {
+            var div = document.createElement('div');
+            div.innerHTML = stripTags(s);
+            return div.childNodes[0] ? div.childNodes[0].nodeValue : s;
+        };
+
+        // Public
+
+        return {
+
+            uploader: null,
+
+            originalFileList: null,
+
+            init: function () {
+                Y.on('domready', function () {
+                    var uploadButton = Y.one('#uploadButton');
+                    var overlay = Y.one('#uploadButtonOverlay');
+
+                    overlay.setStyle('width', uploadButton.right - uploadButton.left + "px");
+                    overlay.setStyle('height', uploadButton.bottom - uploadButton.top + "px");
+
+                    Y.Uploader = Y.UploaderHTML5;
+                    Y.ez.MultiUpload.uploader = new Y.Uploader();
+
+                    if (Y.Uploader.TYPE == "html5") {
+                        Y.ez.MultiUpload.uploader.set("uploadURL", Y.ez.MultiUpload.cfg.uploadURL);
+                        Y.ez.MultiUpload.uploader.set("multipleFiles", Y.ez.MultiUpload.cfg.multipleFiles);
+                        Y.ez.MultiUpload.uploader.set("dragAndDropArea", "#divContainer");
+                        Y.ez.MultiUpload.uploader.render("#uploadButtonOverlay");
+
+                        Y.ez.MultiUpload.uploader.on('fileselect', onFileSelect);
+                        Y.ez.MultiUpload.uploader.on('uploadstart', onUploadStart);
+                        Y.ez.MultiUpload.uploader.on('uploadprogress', onUploadProgress);
+                        Y.ez.MultiUpload.uploader.on('totaluploadprogress', onTotalUploadProgress);
+                        Y.ez.MultiUpload.uploader.on('uploadcomplete', onUploadComplete);
+                        Y.ez.MultiUpload.uploader.on('uploaderror', onUploadError);
+                    }
+                    else {
+                        Y.log("No HTML5 capabilities detected.");
+                    }
+                });
+            },
+
+            cfg: {
+                uploadURL: false,
+                uploadVars: false,
+                allFilesRecived: '',
+                uploadCanceled: '',
+                thumbnailCreated: '',
+                multipleFiles: true
+            }
         }
-        Dom.setStyle('multiuploadProgressBar' , 'width', 0);
-        Dom.get('multiuploadProgressMessage').innerHTML = "";
-
-        for(var i in e.fileList)
-        {
-            this.queue.push(e.fileList[i]);
-        }
-
-        var fileID = this.queue[this.uploadCounter]['id'];
-        this.upload(fileID, YAHOO.ez.MultiUpload.cfg.uploadURL, 'POST', YAHOO.ez.MultiUpload.cfg.uploadVars);
-
-        Dom.get('uploadButton').disabled = true;
-
-        var cancelUploadButton = Dom.get('cancelUploadButton');
-        Dom.setStyle(cancelUploadButton, 'visibility', 'visible');
-
-        Event.on(cancelUploadButton, 'click', cancelUpload, this, true);
-
-        this.disable();
-    };
-
-    var onUploadStart = function(e)
-    {
-        Dom.setStyle('multiuploadProgressBar' , 'width', 0);
-        Dom.get('multiuploadProgressMessage').innerHTML = '';
-        Dom.get('multiuploadProgressFile').innerHTML = (this.uploadCounter + 1) + '/' + this.queue.length;
-        Dom.get('multiuploadProgressFileName').innerHTML = this.queue[this.uploadCounter]['name'];
-    };
-
-    var onUploadProgress = function(e)
-    {
-        var width = Math.floor((YAHOO.ez.MultiUpload.cfg.progressBarWidth * e['bytesLoaded']) / e['bytesTotal']);
-
-        widthAnimate('multiuploadProgressBar', width);
-    };
-
-    var onUploadComplete = function(e)
-    {
-        Dom.get('multiuploadProgressMessage').innerHTML = YAHOO.ez.MultiUpload.cfg.thumbnailCreated;
-        Dom.setStyle('multiuploadProgressBar' , 'width', '100%');
-
-        if (this.uploadCounter < this.queue.length - 1) {
-            this.uploadCounter++;
-
-            var fileID = this.queue[this.uploadCounter]['id'];
-            this.upload(fileID, YAHOO.ez.MultiUpload.cfg.uploadURL, 'POST', YAHOO.ez.MultiUpload.cfg.uploadVars);
-        } else {
-            this.uploadCounter = 0;
-            this.queue = [];
-
-            Dom.get('uploadButton').disabled = false;
-            Dom.get('multiuploadProgressMessage').innerHTML = YAHOO.ez.MultiUpload.cfg.allFilesRecived;
-
-            Dom.setStyle('cancelUploadButton', 'visibility', 'hidden');
-
-            this.enable();
-            this.clearFileList();
-        }
-    };
-
-    var onUploadCompleteData = function(e)
-    {
-        var response = JSON.parse(e.data);
-        var id = response.id;
-        var data = unescapeHTML(response.data);
-
-        var thumbnail = '<div id="thumbnail_' + id + '" class="thumbnail-block" style="opacity:0;" >' + data + '</div>';
-        var thumbnails = Dom.get('thumbnails');
-        thumbnails.innerHTML = thumbnails.innerHTML + thumbnail;
-
-        fadeAnimate('thumbnail_'+ id, 0, 1);
-    };
-
-    var onUploadError = function(e)
-    {
-        alert(event.status);
-    };
-
-    var cancelUpload = function()
-    {
-        this.cancel();
-        this.enable();
-        this.clearFileList();
-
-        this.uploadCounter = 0;
-        this.queue = [];
-
-        Dom.get('multiuploadProgressMessage').innerHTML = YAHOO.ez.MultiUpload.cfg.uploadCanceled;
-        Dom.get('uploadButton').disabled = false;
-    };
-
-    var fadeAnimate = function(elementID, fromValue, toValue)
-    {
-        var anim = new YAHOO.util.Anim(elementID , { opacity: { from: fromValue, to: toValue } }, 0.2);
-        anim.animate();
-    };
-
-    var widthAnimate = function(elementID, toValue)
-    {
-        var anim = new YAHOO.util.Anim(elementID , { width: { to: toValue } } , 0.5);
-        anim.animate();
-    };
-
-    var stripTags = function(s)
-    {
-        return s.replace(/<\/?[^>]+>/gi, '');
-    };
-
-    var unescapeHTML = function(s)
-    {
-        var div = document.createElement('div');
-        div.innerHTML = stripTags(s);
-        return div.childNodes[0] ? div.childNodes[0].nodeValue : s;
-    };
-
-    // Public
-
-    return {
-
-        init: function() {
-            Event.onDOMReady(function()
-            {
-                var uploadButton = Dom.getRegion('uploadButton');
-                var overlay = Dom.get('uploadButtonOverlay');
-
-                Event.addListener( overlay, 'click', missingFlashClick );
-
-                Dom.setStyle(overlay, 'width', uploadButton.right - uploadButton.left + "px");
-                Dom.setStyle(overlay, 'height', uploadButton.bottom - uploadButton.top + "px");
-
-                YAHOO.widget.Uploader.SWFURL = YAHOO.ez.MultiUpload.cfg.swfURL;
-                var uploader = new YAHOO.widget.Uploader('uploadButtonOverlay');
-
-                uploader.on('contentReady', onContentReady);
-                uploader.on('fileSelect', onFileSelect);
-                uploader.on('uploadStart', onUploadStart);
-                uploader.on('uploadProgress', onUploadProgress);
-                uploader.on('uploadComplete', onUploadComplete);
-                uploader.on('uploadCompleteData', onUploadCompleteData);
-                uploader.on('uploadError', onUploadError);
-
-                uploader.uploadCounter = 0;
-                uploader.queue = [];
-            }, this, true );
-        },
-
-        cfg: { swfURL: false,
-               uploadURL: false,
-               uploadVars: false,
-               fileType: false,
-               progressBarWidth: 300,
-               allFilesRecived: '',
-               uploadCanceled: '',
-               thumbnailCreated: '' }
-    }
-})();
+    })();
+});
